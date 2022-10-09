a7fc63a097dfe4a9c7d5dcbb36f920669c871afd
caovandanh <caovandanh2002@gmail.com>
2022-10-09 13:47:35 +0700

    Upload: Responsive Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,14 +3,24 @@ import { MenuItems } from '../menu/MenuItems';
 import './NavbarStyles.css';
 
 class Navbar extends Component {
+    state = { clicked: false };
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked });
+    };
+
     render() {
         return (
             <nav className="NavbarItems">
                 {/* logo */}
                 <h1 className="navbar-logo">Trippy</h1>
 
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+
                 {/* menu */}
-                <ul className="nav-menu">
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
