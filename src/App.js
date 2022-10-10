3da1853e06248437ff21926af01dfa7e3ff3488d
caovandanh <caovandanh2002@gmail.com>
2022-10-10 12:10:43 +0700

    Upload: Route/Switch Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import About from './routes/About';
+import Contact from './routes/Contact';
+import Home from './routes/Home';
+import Service from './routes/Service';
 import './styless.css';
 
 function App() {
     return (
         <div className="App">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/service" element={<Service />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+
             <Navbar />
         </div>
     );
